4a989d62295cf6c3ee6bd37a70aca19b186a2d31
panchei19 <110833799+panchei19@users.noreply.github.com>
2022-09-26 10:40:14 -0300

    Initialize review

---
+++ src/Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureMonitor monitor = new TemperatureMonitor();
+            TemperatureReporter reporter = new TemperatureReporter();
+            reporter.Subscribe(monitor);
+            monitor.GetTemperature();
+        }
+    }
+}
